ae626367d9733b26f88f3a177ebac80962e6ac60
Jesse Holwell <jholwell@capebionics.com>
2026-03-21 18:57:29 +1100

    fix vite config

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
@@ -8,7 +8,7 @@ export default defineConfig({
     include: ["src/**/*.test.ts", "src/**/*.test.tsx"],
     coverage: {
       provider: "v8",
-      include: ["src/models/**"],
+      include: ["src/domain/**"],
       reporter: ["text", "html"]
     }
   }
