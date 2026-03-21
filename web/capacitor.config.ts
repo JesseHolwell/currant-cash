f7cf8965779a7670fa571628fe2a51fc8c97417f
Jesse Holwell <jholwell@capebionics.com>
2026-03-21 21:15:49 +1100

    set ios app config

--- web/capacitor.config.ts
+++ web/capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.currant.app',
+  appId: 'com.jesseholwell.currant',
   appName: 'Currant',
   webDir: 'dist',
 };
